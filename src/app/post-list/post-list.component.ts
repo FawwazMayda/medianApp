017701d4009f8da536be67ccbfbf38892e4d8556
fawwazmayda <maydafawwaz@gmail.com>
2019-10-05 17:41:23 +0700

    Add postlist components with expansion panel

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-
+  posts = [
+    {title:"This is the first post", content:"Hello I'm first post"},
+    {title:"This is the second post", content:"Hello I'm second post"},
+    {title:"This is the third post", content:"Hello I'm third post"}]
   constructor() { }
 
   ngOnInit() {
